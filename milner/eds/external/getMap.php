00f2f7c5c5c3650b333ff32a3a3ecd33644767a6
Bill McMillin <billmcmillin@gmail.com>
2014-10-20 17:53:33 -0400

    update of script files with comments

--- milner/eds/external/getMap.php
+++ milner/eds/external/getMap.php
@@ -3,11 +3,7 @@
 $callNum = htmlspecialchars($_GET["callNum"]);
 $loc = htmlspecialchars($_GET["loc"]);
 
-//echo 'You asked for' . $callNum . ' ' . $loc . '!';
-
-
-
-
+//result page that is displayed when user clicks on a link
 	print "<html>";
 	print "<head>";
 
@@ -26,21 +22,33 @@ $loc = htmlspecialchars($_GET["loc"]);
 	print "<body>";
 	print '<div class="main">';
 
-if(strpos($loc,'Floor 2') == true)
+//begin checking for locations
+
+//each location will call a function that will display a different set of images
+if(strpos($loc,'Floor 2 Shelves') !== false)
 {
 	echo 'on floor 2!';
 }
-elseif(strpos($loc,'Floor 3') == true){
-	echo 'on floor 3!';
+elseif(strpos($loc,'Reference') !== false){
+	  echo 'on Floor 2 Reference. Ask at the desk.';
+		print "<img src='../img/FL2ref.gif' />";
 }
-elseif(strpos($loc,'Floor 4') == true){
-	echo 'on floor 4!';
+elseif(strpos($loc,'Floor 3') !== false){
+		print "<img src='../img/FL3-2012-AE.gif' />";
+
 }
-elseif(strpos($loc,'Floor 5') == true){
+elseif(strpos($loc,'Floor 4') !== false){
+	    fl4StacksMap($callNum);
+}
+elseif(strpos($loc,'Floor 5') !== false){
 	echo 'on floor 5!';
 }
-elseif(strpos($loc,'Floor 6') == true){
-	echo 'on floor 6!';
+elseif(strpos($loc,'Floor 6 Shelves') !== false){
+	fl6StacksMap($callNum);
+}
+elseif(strpos($loc,'Floor 6 Special Collections') !== false){
+	fl6Spc($callNum);
+	
 }
 else{
 	echo 'not sure where it is!';
@@ -48,44 +56,19 @@ else{
 
 
 
-if($loc=='Floor 5 Shelves')
-{
-	echo 'on floor 5!';
-}	
-fl6StacksMap($callNum);
-
-
-
-
-//////////////////BRK stacks		
-function fl6StacksMap($callNum){
+function fl4StacksMap($callNum){
 		
 
-		//	if ($location == 'BK') {
-					//if valid, cut to the first 6 chars and test
-					$location = "Floor 6";
-					//strip the tags to prevent executable code from being passed in
-					$callNum = strip_tags($callNum);
-					//strip the first 19 chars off the call number
-					//$callNum = substr_replace($callNum, '', 0, 19);
-					
-					//the way our catalog passes in call numbers leaves extra space characters
-					//before the call number begins. This gets rid of them. Each catalog may
-					//append different amounts of space to the numbers
-					$subCall = substr($callNum, 0, 20);	
-					print "<p>You searched for $callNum</p>";
-					//remove all whitespace from string - necessary for comparisons
-					//$subCall = preg_replace('/\s+/', '', $subCall);
-					
-					
-					//print "<p>Subcall is: $subCall </p>";
-				
+	$location = "Floor 4";
+	$callNum = strip_tags($callNum);
+	//just use the first 20 chars of call number				
+	$subCall = substr($callNum, 0, 20);	
+	print "<p>You searched for $callNum</p>";
 					
 		
-		// 0.001 - 006.5A - Shelf 1
-			 if ($subCall >= 'A' && $subCall < 'PQ'){
-						print "<img src='../img/FL4GHB.gif' />";
-						//print "<p> $callNum is not on floor 6</p>";
+	 if ($subCall >= 'A' && $subCall < 'PQ'){
+			print "<img src='../img/FL4GHB.gif' />";
+			print "<p> $callNum is on floor 4.</p>";
 		}
 
 			
@@ -118,7 +101,9 @@ function fl6StacksMap($callNum){
 
 					}
 			else{
-				print "<p>Other call number</p>";
+				print "<p>Other call number. Please ask at the reference desk.</p>";
+				print "<img src='../img/FL2ref.gif' />";
+			
 
 			}
     			print "</div>";
@@ -126,12 +111,80 @@ function fl6StacksMap($callNum){
  
 	}	
 	
-			 //end BK function
+			 //end Floor 4 Shelves function
+
+//////////////////Floor 6 Shelves function/////////		
+function fl6StacksMap($callNum){
+		
+
+	$location = "Floor 6";
+					//strip the tags to prevent executable code from being passed in
+	$callNum = strip_tags($callNum);
+	$subCall = substr($callNum, 0, 20);	
+	print "<p>You searched for $callNum</p>";
+					
+	 if ($subCall >= 'A' && $subCall < 'PQ'){
+			print "<img src='../img/FL6-2012-LM.gif' />";
+			print "<p> $callNum is on floor 6.</p>";
+		}
+
+			
+		// PQ PR
+		elseif ($subCall >= 'PQ' && $subCall < 'PR'){
+			print "<img src='img/FL6-2012-PQPR.gif' />";
+			print "<p> $callNum is on floor 6</p>";
+		}
+		
+		// PR PS
+		elseif ($subCall >= 'PR' && $subCall < 'PS'){
+			print "<img src='img/FL6-2012-PRPS.gif' />";
+			print "<p> $callNum is on floor 6</p>";
+		}
+
+		// PS PZ
+		elseif ($subCall >= 'PS0001' && $subCall < 'PZ9999'){
+			print "<img src='img/FL6-2012-PSPZ.gif' />";
+			print "<p> $callNum is on floor 6</p>";
+		}
+		 elseif ($subCall > 'PZ9999'){
+			print "<img src='../img/FL2ref.gif' />";
+			print "<p> $callNum has moved. Please ask at the reference desk.</p>";
+		}
+	
+
+		elseif($subCall > 999.999) {
+			print "<p>Error in call number</p>";
+
+			}
+		else{
+			print "<p>Other call number</p>";
+
+			}
+    	print "</div>";
+	   		
+	}	
+	
+			 //end Floor 6 Shelves function
 		
 		
 		
+			 
+function fl6Spc($callNum){
 		
-			//////////////////Storage basement function		/////////////////////////////
+	$location = "Floor 6";
+	$callNum = strip_tags($callNum);
+				
+	$subCall = substr($callNum, 0, 20);	
+	print "<p><img src='../img/FL6-2012-SpecialCollection.gif' /></p>";
+	print "<p> $callNum is on floor 6 in Special Collections.</p>";
+		
+		
+	}	
+	
+//end Floor 6 Special Collections function
+
+		
+//////////////////Storage basement function		/////////////////////////////
 			
 function storage($callNum){
 					//if valid, cut to the first 6 chars and test
